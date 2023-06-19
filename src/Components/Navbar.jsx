d4a4299f5137275617a02586f5f572c0d31acc86
Bhavya0404 <singhpappu966@gmail.com>
2023-06-20 01:08:39 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { NoEncryption } from '@mui/icons-material';
+
+const drawerWidth = 240;
+const navItems = [
+
+        {   
+            'id': 1,
+            'name': "Find Us",
+            'value': "https://www.google.com/maps/place/Civil+Hospital+(Bahadurgarh,+Haryana)/@28.6964506,76.9237593,17z/data=!3m1!4b1!4m6!3m5!1s0x390d0957257d3b69:0xd0564ce5a6c7ff1e!8m2!3d28.6964506!4d76.9237593!16s%2Fg%2F11bxg9t6sh?entry=ttu",
+        },
+        {
+            'id': 2,
+            'name': "Book Now",
+            'value': "https://wa.me/918745948789",
+        }
+
+]
+
+// const navItems = ['Find Us', 'Book Now'];
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        ZUMAXAA
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.id} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ 
+        display: 'flex',
+    }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{
+        backgroundColor: '#2563EB',
+        height: '6rem',
+        px: '1.5rem',
+      }}>
+        <Toolbar sx={{
+            height: '100%'
+        }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            ZUMAXAA
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <a href={item.value}>
+              <Button key={item.id}  sx={{ color: '#fff', mr: '2px' }}>
+                {item.name}
+              </Button>
+              </a>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
+
+Navbar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default Navbar;
