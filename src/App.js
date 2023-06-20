bb5fd2753225bc076a76b2bdda3b0f61126d6474
Bhavya0404 <singhpappu966@gmail.com>
2023-06-20 21:23:48 +0530

    responsiveness-2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Carousel />
-      {/* <Services /> */}
+      <Services />
       <AboutUs />
       <Footer />
     </div>
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -7,6 +7,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea } from "@mui/material";
+import Divider from "@mui/material/Divider";
 const Services = () => {
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -19,64 +20,64 @@ const Services = () => {
   const data = [
     {
       id: 1,
-      image: require('../assets/images/image1.jpg'),
+      image: require("../assets/images/image1.jpg"),
       heading: "Endodontics",
       text: "This is a sample text",
     },
     {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
-      {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
-      {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
-      {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
-      {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
-      {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
-      {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
-      {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
-      {
-        id: 1,
-        image: require('../assets/images/image1.jpg'),
-        heading: "Endodontics",
-        text: "This is a sample text",
-      },
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
+    {
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
+    {
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
+    {
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
+    {
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
+    {
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
+    {
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
+    {
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
+    {
+      id: 1,
+      image: require("../assets/images/image1.jpg"),
+      heading: "Endodontics",
+      text: "This is a sample text",
+    },
   ];
   return (
     <Box
@@ -85,12 +86,14 @@ const Services = () => {
       }}
     >
       <Typography
-        variant="h3"
         component="div"
         sx={{
           width: "100%",
           textAlign: "center",
-          my: '20px',
+          my: "20px",
+          fontSize: {
+            xs: '2rem',
+          }
         }}
       >
         Services
@@ -99,40 +102,63 @@ const Services = () => {
       <Box
         sx={{
           width: "100%",
-          px: "5.4rem",
+          px: {
+            xs: '2rem',
+            lg: '5.4rem',
+          },
           pt: "20px",
         }}
       >
         <Grid
           container
-          spacing={2}
+          spacing={5}
           sx={{
             mb: "26px",
           }}
         >
           {data.map((d) => (
-            <Grid item xs={2.4}>
-              <Card sx={{ maxWidth: 300, }}>
+            <Grid item xs={6} sm={4} md={3} lg={2.4}>
+              <Box sx={{ maxWidth: 300 }}>
                 <CardActionArea>
                   <CardMedia
                     component="img"
-                    height="140"
+                    // height="140px"
                     image={d.image}
                     alt="green iguana"
+                    sx={{
+                      height:{
+                        // lg: "140px",
+                        // md: "100px",
+                      }
+                    }}
                   />
+
                   <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography
+                      gutterBottom
+                      sx={{ 
+                      textAlign: "center", 
+                      fontSize: {
+                        // lg: '1.5rem',
+                        md: '1.4rem',
+                        sm: '1.3rem',
+                        xs: '1rem',
+                      }, 
+                      }}
+                      // variant="h5"
+                      component="div"
+                    >
                       {d.heading}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
+                    {/* <Typography variant="body2" color="text.secondary">
                       {d.text}
-                    </Typography>
+                    </Typography> */}
                   </CardContent>
                 </CardActionArea>
-              </Card>
+              </Box>
+              
             </Grid>
           ))}
-
         </Grid>
       </Box>
     </Box>
