6874cb34d65a45d375b58121b5daee08b79cf312
Bhavya0404 <singhpappu966@gmail.com>
2023-06-21 00:01:32 +0530

    stage-1-done

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -2,16 +2,9 @@ import { Box, Typography } from "@mui/material";
 import { useState } from "react";
 import React from "react";
 import Rating from "@mui/material/Rating";
-import { useTheme } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
+import Carousel from "react-material-ui-carousel";
+import RatingText from "./RatingText";
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const Reviews = () => {
   return (
@@ -19,25 +12,41 @@ const Reviews = () => {
       sx={{
         display: "flex",
         width: '100%',
+        px: "2.5rem",
+        py: "4rem",
+        flexDirection:{
+            xs: 'column',
+            sm: 'row',
+        }
       }}
     >
       <Box sx={{
-        width: '40%',
+        width: {
+            md: '40%',
+            xs: '80%',
+        },
+        mb: {
+            xs: '5rem',
+            sm: '0',
+        }      
       }}>
-        <Typography variant="h3" component="div" sx={{ mb: "2.5rem" }}>
+        <Typography variant="h4" component="div" sx={{ mb: "2.5rem" }}>
           Kind Words from our Customers
         </Typography>
 
         <Rating value="5" />
-        <a href="https://www.google.com/search?q=zumaxaa&oq=zumaxaa&aqs=chrome..69i57.1505j0j4&sourceid=chrome&ie=UTF-8#lrd=0x390d091bd62f918b:0x6898d259594a4f53,1,,,,">
-        <Typography component="div">160+ reviews</Typography>
+        <a target="_blank" rel="noreferrer" href="https://www.google.com/search?q=zumaxaa&oq=zumaxaa&aqs=chrome..69i57.1505j0j4&sourceid=chrome&ie=UTF-8#lrd=0x390d091bd62f918b:0x6898d259594a4f53,1,,,,">
+        <Typography variant="subtitle1" component="div">160+ reviews</Typography>
         </a>
       </Box>
 
       <Box sx={{
-        width: '60%',
+        width: {
+            md: '60%',
+            xs: '100%',
+        },
       }}>
-            
+            <RatingText />
       </Box>
     </Box>
   );
